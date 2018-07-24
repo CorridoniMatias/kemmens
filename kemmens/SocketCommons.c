5926b34fbc64cb2dbd741b184d7ce8678c17cc3d
Matias Mendez Corridoni <matim97@gmail.com>
2018-07-24 14:19:16 -0300

    Socket commons en kemmens

--- kemmens/SocketCommons.c
+++ kemmens/SocketCommons.c
@@ -1,6 +1,21 @@
 #include "kemmens/SocketCommons.h"
 
+ContentHeader* SocketCommons_CreateHeader()
+{
+	return (ContentHeader*)malloc(sizeof(ContentHeader));
+}
+
 int SocketCommons_SendMessageString(int socket, char* message)
 {
 	return send(socket, message, string_length(message), 0);
+}
+
+int SocketCommons_ReceiveHeader(int socket)
+{
+	//Creamos el header que nos va a dar el lengh del tamaño de lo que sea que vamos a recibir posteriormente en el "body"
+	ContentHeader * header = SocketCommons_CreateHeader();
+	//Recibimos el Header, al ser un struct compartido, tanto el server como el client lo conocen y saben crearlo para informarle al otro el tamaño del mensaje que van a mandar.
+	recv(socket, header, sizeof(ContentHeader), 0);
+
+
 }
--- kemmens/kemmens/SocketCommons.h
+++ kemmens/kemmens/SocketCommons.h
@@ -4,6 +4,14 @@
 #include "SocketCommons.h"
 #include "commons/string.h"
 
+
+typedef struct {
+  int body_length;
+} __attribute__((packed)) ContentHeader;
+
 int SocketCommons_SendMessageString(int socket, char* message);
+int SocketCommons_ReceiveHeader(int socket);
+ContentHeader* SocketCommons_CreateHeader();
+char* SocketCommons_ReceiveString(int socket);
 
 #endif /* SOCKETCOMMONS_H_ */
