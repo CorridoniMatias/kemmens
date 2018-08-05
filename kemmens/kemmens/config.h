1c0eccfd8a025e63f3d653fd984f52dcf811a1cf
juanbanchero <juan.banche@gmail.com>
2018-08-05 19:31:51 -0300

    Se crea el .h de las kemmens de config

---
+++ kemmens/kemmens/config.h
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <commons/config.h>
+
+
+//Funciones de archivos de configuracion
+t_config* archivoConfigCrear(char* path, char** campos);
+bool archivoConfigTieneCampo(t_config* archivoConfig, char* campo);
+bool archivoConfigFaltaCampo(t_config* archivoConfig, char* campo);
+char* archivoConfigSacarStringDe(t_config* archivoConfig, char* campo);
+int archivoConfigSacarIntDe(t_config* archivoConfig, char* campo);
+long archivoConfigSacarLongDe(t_config* archivoConfig, char* campo);
+double archivoConfigSacarDoubleDe(t_config* archivoConfig, char* campo);
+char** archivoConfigSacarArrayDe(t_config* archivoConfig, char* campo);
+int archivoConfigCantidadCampos(t_config* archivoConfig);
+void archivoConfigDestruir(t_config* archivoConfig);
+void archivoConfigSetearCampo(t_config* archivoConfig, char* campo, char* valor);
+bool archivoConfigInvalido(t_config* archivoConfig, char** campos);
+bool archivoConfigInexistente(t_config* archivoConfig);
+bool archivoConfigIncompleto(t_config* archivoConfig, char** campos);
