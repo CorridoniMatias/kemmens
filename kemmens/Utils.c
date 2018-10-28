46a5caf091a5fb023499a4d66cb6583bfd6a62ec
Matias Mendez Corridoni <matim97@gmail.com>
2018-10-28 00:10:33 -0300

    Agrego Utils que contiene una funcion de hash md5

---
+++ kemmens/Utils.c
@@ -0,0 +1,13 @@
+#include "kemmens/Utils.h"
+
+
+unsigned char* KemmensUtils_md5(void* content)
+{
+	void * digest = malloc(MD5_DIGEST_LENGTH);
+	MD5_CTX context;
+	MD5_Init(&context);
+	MD5_Update(&context, content, strlen(content) + 1);
+	MD5_Final(digest, &context);
+
+	return digest;
+}
---
+++ kemmens/kemmens/Utils.h
@@ -0,0 +1,9 @@
+#ifndef KEMMENS_UTILS_H_
+#define KEMMENS_UTILS_H_
+
+#include <openssl/md5.h>
+
+
+unsigned char* KemmensUtils_md5(void* content);
+
+#endif /* KEMMENS_UTILS_H_ */
