f65b7aa4957a18c99b7ac059f4a2ecd90a2df411
Matias Mendez Corridoni <matim97@gmail.com>
2018-12-03 22:41:29 -0300

    Borrada basura del serialization y agregada doc de megekemmen

--- kemmens/kemmens/megekemmen.h
+++ kemmens/kemmens/megekemmen.h
@@ -1,15 +1,21 @@
 #ifndef MEGEKEMMEN_H_
 #define MEGEKEMMEN_H_
-//MegaCommons <> inclusive language
-
-/*
- *		INCLUIR ESTE ARCHIVO PARA ACCEDER A LAS FUNCIONALIDADES BASICAS DE LAS KEMMENS
- */
 
 #include <stdio.h>
 #include <stdlib.h>
 
-void exit_gracefully(int);
-void exit_gracefully_custom(void (*operations)(),int);
+/**	void exit_gracefully(int);
+ * 		ACCION: Destruye el CommandInterpreter y el log, y finaliza el programa	void exit_gracefully_custom(void (*operations)(),int);
+ * 		PARAMETROS:
+ * 			exit_code: Codigo de salida (0 en caso de exito)
+ */
+void exit_gracefully(int exit_code);
+ /**
+ * 		ACCION: Ejecuta una cierta funcion, y luego destruye el CommandInterpreter y el log, y finaliza el programa
+ * 		PARAMETROS:
+ * 			operations(): Funcion (que no recibe parametros) a ejecutar previo a terminar
+ * 			exit_code: Codigo de salida (0 en caso de exito)
+ */
+void exit_gracefully_custom(void (*operations)(), int exit_code);
 
 #endif /* MEGEKEMMEN_H_ */
